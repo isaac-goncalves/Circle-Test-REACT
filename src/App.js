4c65f0d8e949ec17f561b4c9bc0e218580aedc97
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-06 13:06:46 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
 
 import './App.css';
 import { useState } from 'react'
-
-const getClickPosition = (e) => {
-  console.log(e)
-  const X = e.clientX;
-  const Y = e.clientY;
- 
-  return [X, Y];
-
-}
-
-function createCilcle(event) {
-  console.log('Clicou');
-  // return (
-  //   <div class_ame='circle'></div>
-  // )
-  const [X, Y] = getClickPosition(event);
-  console.log(Y);
-  console.log(X);
-
-  // const newCirclesArray = [...circlesArray, {X, Y}];
-
-}
-
-
-
+import randomcolor from 'randomcolor'
 
 function App() {
-  const [circles,setCircles] = useState([]) 
+  const [circles, setCircles] = useState([])
+  const [deletedCircles, setDeletedCircles] = useState([])
+
+  const getClickPosition = (e) => {
+
+    const dim = e.target.getBoundingClientRect()
+    const X = e.clientX - dim.left;
+    const Y = e.clientY - dim.top;
+    return [X, Y];
+  }
+
+  const deleteCircle = () => {
+
+    if (circles.length > 0) {
+      const circle = circles.pop()
+      setDeletedCircles([...deletedCircles, circle])
+      setCircles([...circles])
+    }
+
+  }
+
+  const restoreCircle = () => {
+
+    if (deletedCircles.length > 0) {
+      const circle = deletedCircles.pop()
+      setCircles([...circles, circle])
+      setDeletedCircles([...deletedCircles])
+    }
+
+  }
+
+
+  function createCircle(event) {
+    console.log('Clicou');
+
+    const [X, Y] = getClickPosition(event);
+    console.log(Y);
+    console.log(X);
+
+    let newCircle = (
+      <circle
+        className='circle'
+        key={Math.random(
+        )}
+        cx={X}
+        cy={Y}
+        r="10"
+        stroke="black"
+        strokeWidth="1"
+        fill={randomcolor()}
+      />
+    )
+
+    let allCircles = [...circles, newCircle];
+
+    console.log("allCircles", allCircles)
+    setCircles(allCircles);
+  }
 
   return (
     <div className="App">
       <h1>Clique em qualquer lugar para criar um circulo</h1>
-      <div onClick={createCilcle} className='circle-area'>Area clicavel
-        <div className='circle'></div></div>
+      <button onClick={deleteCircle}>
+        Undo
+      </button>
+      <button onClick={restoreCircle}>
+        Redo
+      </button>
+      <div onClick={createCircle} className='circle-area'>
+        <svg width='100%' height='100%'>
+          {circles}
+        </svg>
+      </div>
     </div>
   );
 }
